4da3ae12059119fa814120724b6d64a702ba6ba8
Nepolean23 <nlean1996@gmail.com>
2022-12-01 13:54:17 +0530

    FlipCoin Program

---
+++ FlipCoin/FlipCoin/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace FlipCoin
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int head = 0;
+            int tail = 0;
+            int headPer;
+            int tailPer;
+            Console.WriteLine("Flipping of coin");
+            Console.WriteLine("Enter a value for number of flips");
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < val1; i++)
+            {
+                Random random = new Random();
+                int coin = random.Next(1, 3);
+                if (coin == 1)
+                {
+                    Console.WriteLine("Its head");
+                    head++;
+                }
+                else
+                {
+                    Console.WriteLine("Its tail");
+                    tail++;
+                }
+            }
+            headPer = head * 100 / val1;
+            tailPer = tail * 100 / val1;
+            Console.WriteLine("total no of head count: " + head);
+            Console.WriteLine("total no of tail count: " + tail);
+            Console.WriteLine("Percentage of head: " + headPer + "%");
+            Console.WriteLine("Percentage of tail: " + tailPer + "%");
+        }
+    }
+
+}
